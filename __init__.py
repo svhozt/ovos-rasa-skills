88cd169afef4c466e1431c523d73e0583f5d2418
ravindukathri <ravindukathri@gmail.com>
2023-11-24 16:10:03 +0100

    update

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from ovos_workshop.skills.ovos import OVOSSkill
 from ovos_workshop.decorators import intent_handler
+from ovos_utils.intents import IntentBuilder
 import socketio
 
 
@@ -29,18 +30,24 @@ class RasaSocketClient:
 
 
 class RasaSkill(OVOSSkill):
-    def __init__(self):
-        super(RasaSkill, self).__init__("RasaSkill")
-        # Update this URL to point to your Rasa server
+    def __init__(self, *args, **kwargs):
+        """The __init__ method is called when the Skill is first constructed.
+        Note that self.bus, self.skill_id, self.settings, and
+        other base class settings are only available after the call to super().
+
+        This is a good place to load and pre-process any data needed by your
+        Skill, ideally after the super() call.
+        """
+        super().__init__(*args, **kwargs)
+    # def __init__(self):
+    #     super(RasaSkill, self).__init__("RasaSkill")
+    #     # Update this URL to point to your Rasa server
         self.rasa_client = RasaSocketClient("http://host.docker.internal:5005")
 
-    @intent_handler('ask_rasa.intent')
+    @intent_handler(IntentBuilder('TalkToRasa').require('TalkToRasa'))
     def handle_ask_rasa_intent(self, message):
         user_utterance = message.data.get('utterance')
         rasa_response = self.rasa_client.send_to_rasa(user_utterance)
         self.speak(rasa_response)
 
-
-def create_skill():
-    return RasaSkill()
 
