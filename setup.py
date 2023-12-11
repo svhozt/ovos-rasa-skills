7889eb233ac15edcff625c70235bd99781e8aac8
ravindukathri <57557859+ravindukathri@users.noreply.github.com>
2023-12-11 14:15:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os import walk, path
 
 URL = "https://github.com/ravindukathri/ovos-rasa-skill"
 SKILL_CLAZZ = "OVOSRasaSkill"  # needs to match __init__.py class name
-PYPI_NAME = "ovos_rasa_skill"  # pip install PYPI_NAME
+PYPI_NAME = "ovos-skill-rasa"  # pip install PYPI_NAME  ##Changed from ovos_rasa_skill
 
 # below derived from github url to ensure standard skill_id
 SKILL_AUTHOR, SKILL_NAME = URL.split(".com/")[-1].split("/")
