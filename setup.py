0d7730f8535758e0e9a2b8360c3027549d59fc53
ravindukathri <57557859+ravindukathri@users.noreply.github.com>
2023-12-11 14:25:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,12 +15,41 @@ SKILL_PKG = SKILL_NAME.lower().replace('-', '_')
 PLUGIN_ENTRY_POINT = f'{SKILL_NAME.lower()}.{SKILL_AUTHOR.lower()}={SKILL_PKG}:{SKILL_CLAZZ}'
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-def required(requirements_file):
-    """Read requirements file and remove comments and empty lines."""
-    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
-        requirements = f.read().splitlines()
-        return [pkg for pkg in requirements if pkg.strip() and not pkg.startswith("#")]
+def find_resource_files():
+    resource_base_dirs = ("locale", "ui", "vocab", "dialog", "regex", "res")
+    base_dir = BASE_PATH
+    package_data = ["skill.json"]
+    for res in resource_base_dirs:
+        if path.isdir(path.join(base_dir, res)):
+            for (directory, _, files) in walk(path.join(base_dir, res)):
+                if files:
+                    package_data.append(
+                        path.join(directory.replace(base_dir, "").lstrip('/'),
+                                  '*'))
+#    print(package_data)
+    return package_data
 
+
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(BASE_PATH, requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.strip() for r in requirements if r.strip()
+                    and not r.strip().startswith("#")]
+
+    for i in range(0, len(requirements)):
+        r = requirements[i]
+        if "@" in r:
+            parts = [p.lower() if p.strip().startswith("git+http") else p
+                     for p in r.split('@')]
+            r = "@".join(parts)
+        if getenv("GITHUB_TOKEN"):
+            if "github.com" in r:
+                requirements[i] = \
+                    r.replace("github.com",
+                              f"{getenv('GITHUB_TOKEN')}@github.com")
+    return requirements
+    
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -37,13 +66,13 @@ setup(
     url=URL,  # Replace with your repository URL
     package_dir={SKILL_PKG: ""},
     packages=[SKILL_PKG],
-    package_data={SKILL_PKG: required()},
+    package_data={SKILL_PKG:find_resource_files()},
     author='Your Name',
     author_email='your.email@example.com',
     license='Apache-2.0',
     zip_safe=True, #
     include_package_data=True,
-    install_requires=required("requirements.txt"),
+    install_requires=get_requirements("requirements.txt"),
     long_description="An OVOS skill for integrating with Rasa using Socket.IO",
     long_description_content_type='text/markdown',
     keywords='ovos skill plugin',
